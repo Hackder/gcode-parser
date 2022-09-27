306cb960b5acf56d005323fe149ab975db4c0d2b
Juraj Petráš <jurkopetras@gmail.com>
2022-09-27 18:45:47 +0200

    Remember window size

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,18 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
+// use tauri::Manager;
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        // .setup(|app| {
+        //     let window = app.get_window("main").unwrap();
+        //     window.open_devtools();
+        //     Ok(())
+        // })
+        .plugin(tauri_plugin_window_state::Builder::default().build())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
